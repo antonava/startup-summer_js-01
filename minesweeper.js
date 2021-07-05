7d409463f37f771102e01eed95740cccb4e664ef
Anastasiya <anastasiya.antonava@gmail.com>
2021-07-05 23:56:34 +0300

    add some code

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,6 @@
 //In this exercise you have to create some code that 
 //counts the number of mines adjacent to a given empty square and replaces that square with the count
 
-// let array = [
-//     ['', '*', '', '*', ''],
-//     ['', '', '*', '', ''],
-//     ['', '', '*', '', ''],
-//     ['', '', '' , '', '']
-// ];
-
-
 //1 - mine
 
 let arr = [
@@ -19,15 +11,25 @@ let arr = [
 ];
 
 
-
-for (let i = 0; i <= arr.length - 1; i++) {
-    for(let j = 0; j <= arr[i].length - 1; j++) {
+let mark = [];
+for (let i = 0; i <= arr.length - 1; i++) { //row
+    mark[i] = [];
+    for(let j = 0; j <= arr[i].length - 1; j++) { //col
         if(arr[i][j] == 1) {
-         
+         mark[i][j] = 10;
         } else {
             let count = 0;
             if(arr[i - 1] && arr[i - 1][j - 1] === 1) count ++;
-            if()
+            if(arr[i - 1] && arr[i - 1][j] === 1) count ++;
+            if(arr[i - 1] && arr[i - 1][j + 1] === 1) count ++;
+            if(arr[i][j - 1] === 1) count ++;
+            if(arr[i][j + 1] === 1) count ++;
+            if(arr[i + 1] && arr[i + 1][j - 1] === 1) count ++;
+            if(arr[i + 1] && arr[i + 1][j] === 1) count ++;
+            if(arr[i + 1] && arr[i + 1][j + 1] === 1) count ++;
+            mark[i][j] = count;
         }
     }
 }
+
+console.log(mark);
